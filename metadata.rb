da02acef8a22c677d5067a41259f9b61bf47725c
Eugene Hlyzov <hlyzov@gmail.com>
2014-10-01 14:28:44 +0400

    first public version

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'lazy-brigade'
+maintainer       'Eugene Hlyzov'
+maintainer_email 'hlyzov@gmail.com'
+license          'All rights reserved'
+description      'These guys can do some rails, but do not expect too much'
+version          '0.2.4'
+
+depends          'sudo'
+depends          'nginx'
