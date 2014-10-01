26bba03ce2ccd9d471ae5ad9995b0dc1b426e148
Eugene Hlyzov <ehlyzov@users.noreply.github.com>
2014-10-01 15:19:14 +0400

    No rights reserved

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'lazy-brigade'
 maintainer       'Eugene Hlyzov'
 maintainer_email 'hlyzov@gmail.com'
-license          'All rights reserved'
+license          'MIT'
 description      'These guys can do some rails, but do not expect too much'
 version          '0.2.4'
 
