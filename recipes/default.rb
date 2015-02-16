5a998877450b760a1da5f078f2419db42e3119f2
Eugene Hlyzov <hlyzov@gmail.com>
2015-02-16 14:27:47 +0300

    [feature] allow configuration only by node attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,14 @@
 node['ext-packages'].each do |pkg|
   package pkg
 end
+
+if apps = node['lazy-brigade-apps']
+  apps.each do |app|
+    lazy_brigade node[app]['domain'] do
+      user node[app]['user']
+      environment node[app]['environment']
+      unicorn_workers node[app]['unicorn-workers']
+      app_secrets node[app]['secrets']
+    end
+  end
+end
